6e929aef109bf4ad4e947ba866e2a028a8f09eec
David <cruz-azul99@hotmail.com>
2022-10-11 11:05:00 -0500

    Data filter file created

---
+++ data_filter.py
@@ -0,0 +1,90 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+
+def run():
+  all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+  all_platzi_workes = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+
+  all_adults_workers = list(filter(lambda worker: worker["age"] > 18, DATA))
+  adults = list(map(lambda worker: worker["name"], all_adults_workers))
+
+  # El símbolo | sirve para unir diccionarios, sólo funciona en la versión 3.9 en adelante
+  old_workers = list(map(lambda worker: worker | { "old": worker["age"] > 70 }, DATA))
+  
+  return old_workers
+
+
+if __name__ == '__main__':
+  result = run()
+  print(result)
